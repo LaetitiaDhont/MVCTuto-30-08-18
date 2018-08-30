8ecec3189ab956f15f1e2ec128ac525d0b9c4106
Dhont Laetitia <laetitiadhont75@gmail.com>
2018-08-30 16:58:34 +0200

    Add comments

--- TutoMVC-30-08-18/src/package1/MVCPatternDemo.java
+++ TutoMVC-30-08-18/src/package1/MVCPatternDemo.java
@@ -2,9 +2,18 @@ package package1;
 
 public class MVCPatternDemo {
 
+	
 	public static void main(String[] args) {
 
+		// Récupérer les infos de l'étudiant comme si on avait qu'un seul étudiant dans
+		// notre BDD.
+		// Ici on simule notre BDD
+		
+		// model est un objet de type Student
+
 		Student model = retriveStudentFromDatabase();
+		
+		// view est un objet de type StudentView on simule la partie view du pc
 
 		StudentView view = new StudentView();
 
@@ -15,11 +24,15 @@ public class MVCPatternDemo {
 		// update model data
 
 		controller.setStudentName("Pierre");
+		
+		System.out.println("name :" + model.getName());
 
 		controller.updateView();
 
 	}
 
+	// Cette méthode sert à simuler la BDD
+	
 	private static Student retriveStudentFromDatabase() {
 		Student student = new Student();
 		student.setName("Hamed");
--- TutoMVC-30-08-18/src/package1/StudentController.java
+++ TutoMVC-30-08-18/src/package1/StudentController.java
@@ -1,31 +1,36 @@
 package package1;
 
+/*
+ * We are going to create a Student object acting as a model.StudentView will be a view class which can print student details on console and StudentController is the controller class responsible to store data in Student object and update view StudentView accordingly.
+
+MVCPatternDemo, our demo class, will use StudentController to demonstrate use of MVC pattern.
+ */
 public class StudentController {
 
 	private Student model;
 	private StudentView view;
-	
+
 	public StudentController(Student model, StudentView view) {
 		this.model = model;
 		this.view = view;
 	}
-	
+
 	public void setStudentName(String name) {
 		model.setName(name);
 	}
-	
+
 	public String getStudentName() {
 		return model.getName();
 	}
-	
+
 	public void setStudentRollNo(String rollNo) {
 		model.setRollNo(rollNo);
 	}
-	
+
 	public String getStudentRollNo() {
 		return model.getRollNo();
 	}
-	
+
 	public void updateView() {
 		view.printStudentDetails(model.getName(), model.getRollNo());
 	}
